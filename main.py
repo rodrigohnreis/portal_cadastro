747f9852275906cf1e54b4b01d141922a59c3367
rodrigohnreis <rodrigohnreis@gmail.com>
2025-06-14 19:01:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -28,11 +28,11 @@ app.config['SECRET_KEY'] = 'asdf#FGSgvasgf$5$WGT'
 #app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:12345Ab@localhost:5432/portal_cadastro"
 # Caso queira usar SQLite, descomente a linha abaixo:
 # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///portal_cadastro.db"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:12345Ab@localhost:5432/portal_cadastro"
-#import os
-#from dotenv import load_dotenv
-#load_dotenv()
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:12345Ab@localhost:5432/portal_cadastro"
+import os
+from dotenv import load_dotenv
+load_dotenv()
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
